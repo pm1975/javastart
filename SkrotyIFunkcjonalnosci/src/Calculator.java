1b168f74f1bf898452d8999c75f09ac3eadebb43
pm1975 <pm1975@windowslive.com>
2019-09-04 21:50:31 +0200

    Finish part SkrotyIFunkcjonalnosci

--- SkrotyIFunkcjonalnosci/src/Calculator.java
+++ SkrotyIFunkcjonalnosci/src/Calculator.java
@@ -2,5 +2,7 @@
  * Created by Piotr on 2019-09-03
  */
 public class Calculator {
-    
+    public int add(int a, int b){
+        return a + b;
+    }
 }
--- SkrotyIFunkcjonalnosci/src/HelloWorld.java
+++ SkrotyIFunkcjonalnosci/src/HelloWorld.java
@@ -3,6 +3,8 @@
  */
 public class HelloWorld {
     public static void main(String[] args) {
-
+        Calculator calculator = new Calculator(); //Option + Enter
+        int wynik = calculator.add(5, 10);
+        System.out.println(wynik);
     }
 }
