0c7d34787931d98c3f85b055e35454013d9f7d10
pm1975 <pm1975@windowslive.com>
2019-09-24 21:26:16 +0200

    Library v0.4

--- library/src/Book.java
+++ library/src/Book.java
@@ -10,12 +10,16 @@ class Book {
     String isbn;
 
     Book(String title, String author, int releaseDate, int pages, String publisher, String isbn) {
+        this(title, author, releaseDate, pages, publisher);
+        this.isbn = isbn;
+    }
+
+    public Book(String title, String author, int releaseDate, int pages, String publisher) {
         this.title = title;
         this.author = author;
         this.releaseDate = releaseDate;
         this.pages = pages;
         this.publisher = publisher;
-        this.isbn = isbn;
     }
 
     void printInfo() {
--- library/src/Library.java
+++ library/src/Library.java
@@ -3,25 +3,7 @@
  */
 public class Library {
     public static void main(String[] args) {
-//        final String appName = "Biblioteka v0.2";
-//
-//        Book book1 = new Book();
-//        book1.title = "W pustyni i w puszczy";
-//        book1.author = "Henryk Sienkiewicz";
-//        book1.releaseDate = 2010;
-//        book1.pages = 296;
-//        book1.publisher = "Greg";
-//        book1.isbn = "9788373271890";
-//
-//        System.out.println(appName);
-//        System.out.println("Książki w bibliotece:");
-//        System.out.println(book1.title);
-//        System.out.println(book1.author);
-//        System.out.println(book1.releaseDate);
-//        System.out.println(book1.pages);
-//        System.out.println(book1.publisher);
-//        System.out.println(book1.isbn);
-        final String appName = "Biblioteka v0.3";
+        final String appName = "Biblioteka v0.4";
 
         System.out.println(appName);
         System.out.println("Książki dostępne w bibliotece:");
@@ -30,7 +12,7 @@ public class Library {
         Book book2 = new Book("Java. Efektywne programowanie. Wydanie II", "Joshua Bloch", 2009,
                 352, "Helion", "9788324620845");
         Book book3 = new Book("Pismo Święte starego i nowego testamentu", "Zespół biblistów polskich",
-                1980, 1439, "Pallottinum", "nie ma");
+                1980, 1439, "Pallottinum");
         book1.printInfo();
         book2.printInfo();
         book3.printInfo();
