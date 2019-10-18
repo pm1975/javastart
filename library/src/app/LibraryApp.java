340a712b72ee64cad294e5b947d7b64c51916a1f
PM <pm1975@windowslive.com>
2019-10-18 10:06:50 +0200

    library with static variables

--- library/src/app/LibraryApp.java
+++ library/src/app/LibraryApp.java
@@ -8,8 +8,8 @@ import model.Book;
  */
 public class LibraryApp {
     public static void main(String[] args) {
-        final String appName = "Biblioteka v0.9";
-        System.out.println(appName);
+        private static final String APP_NAME = "Biblioteka v1.0";
+        System.out.println(APP_NAME);
         LibraryControl libraryControl = new LibraryControl();
         libraryControl.controlLoop();
     }
