32da8ee3e2f4c427194e46b0ea5cb0bae4bcaca8
PM <pm1975@windowslive.com>
2019-11-02 19:45:46 +0100

    exceptions try catch

---
+++ 29-wyjatki/src/Test.java
@@ -0,0 +1,23 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class Test {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Podaj liczbę.");
+        int number = 0;
+        boolean error = true;
+        do {
+            try {
+                number = sc.nextInt();
+                error = false;
+                System.out.println("Wczytano poprawną liczbę");
+            } catch (InputMismatchException ex) {
+                System.err.println("Nie podałeś liczby całkowitej, spróbuj jeszcze raz: ");
+                sc.nextLine();
+            }
+        } while (error);
+        System.out.println("Podałeś: " + number);
+        sc.close();
+    }
+}
---
+++ 29-wyjatki/src/TestMultyCatch.java
@@ -0,0 +1,31 @@
+import java.util.InputMismatchException;
+import java.util.Scanner;
+
+public class TestMultyCatch {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int[] numbers = new int[2];
+        boolean error = true;
+        do {
+            try {
+                System.out.println("Podaj 1 liczbę: ");
+                numbers[0] = sc.nextInt();
+                sc.nextLine();
+                System.out.println("Podaj 2 liczbę: ");
+                numbers[1] = sc.nextInt();
+                sc.nextLine();
+
+                System.out.println("Którą wartość wyświetlić (1 lub 2)?");
+                System.out.println("Liczba: " + numbers[sc.nextInt() - 1]);
+                error = false;
+            } catch (InputMismatchException ex) {
+                System.out.println("Nie podałeś liczby całkowitej, spróbuj jeszcze raz: ");
+            } catch (ArrayIndexOutOfBoundsException ex) {
+                System.out.println("Miało być 1 lub 2, zacznijmy od nowa: ");
+            } finally {
+                sc.nextLine(); //wykona się zawsze, niezależnie, czy wyjątki wystąpią, czy nie
+            }
+        } while (error);
+        sc.close();
+    }
+}
