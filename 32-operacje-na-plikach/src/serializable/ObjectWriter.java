6e4b8d3cc913f68055ac2e58a7cc6c621adf2ece
PM <pm1975@windowslive.com>
2019-11-09 01:44:41 +0100

    32 operacje na plikach serializable

---
+++ 32-operacje-na-plikach/src/serializable/ObjectWriter.java
@@ -0,0 +1,23 @@
+package src.serializable;
+
+import java.io.FileNotFoundException;
+import java.io.FileOutputStream;
+import java.io.IOException;
+import java.io.ObjectOutputStream;
+
+public class ObjectWriter {
+    public static void main(String[] args) {
+        String fileName = "person.obj";
+        Person p1 = new Person("Jan", "Kowalski");
+
+        try (
+                var fs = new FileOutputStream(fileName);
+                var os = new ObjectOutputStream(fs);
+        ) {
+            os.writeObject(p1);
+            System.out.println("Zapisano obiekt do pliku");
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
