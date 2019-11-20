45a7f95e5c78fd06d83c1c39e87a2e35c8d53683
PM <pm1975@windowslive.com>
2019-11-20 15:32:49 +0100

    37 comparable

---
+++ 37-comparable-i-comparator/src/Product.java
@@ -0,0 +1,53 @@
+public class Product implements Comparable<Product> {
+    private String producer;
+    private String name;
+    private String category;
+
+    public String getProducer() {
+        return producer;
+    }
+
+    public void setProducer(String producer) {
+        this.producer = producer;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getCategory() {
+        return category;
+    }
+
+    public void setCategory(String category) {
+        this.category = category;
+    }
+
+    public Product(String producer, String name, String category) {
+        this.producer = producer;
+        this.name = name;
+        this.category = category;
+    }
+
+    @Override
+    public String toString() {
+        return "Product [category=" + category + ", producer=" + producer + ", name=" + name + "]";
+    }
+
+    @Override
+    public int compareTo(Product p) {
+        int categoryCompare = category.compareTo(p.getCategory());
+        if (categoryCompare != 0) {
+            return categoryCompare;
+        }
+        int producerCompare = producer.compareTo(p.getProducer());
+        if (producerCompare != 0) {
+            return producerCompare;
+        }
+        return name.compareTo(p.getName());
+    }
+}
---
+++ 37-comparable-i-comparator/src/ProductCatalog.java
@@ -0,0 +1,26 @@
+import javax.swing.*;
+import java.util.Arrays;
+
+public class ProductCatalog {
+    public static void main(String[] args) {
+        Product[] products = new Product[7];
+        products[0] = new Product("Amino", "Zupa pomidorowa", "Zupy");
+        products[1] = new Product("Amino", "Zupa ogórkowa", "Zupy");
+        products[2] = new Product("WINIARY", "Zupa pomidorowa", "Zupy");
+        products[3] = new Product("WINIARY", "Zupa pomidorowa", "Zupy błyskawiczne");
+        products[4] = new Product("WINIARY", "Rosół", "Zupy");
+        products[5] = new Product("Knorr", "Placki ziemniaczane", "Dania obiadowe");
+        products[6] = new Product("Knorr", "Racuchy", "Dania obiadowe");
+
+        System.out.println("Nieposortowana: ");
+        for (Product p : products) {
+            System.out.println(p);
+        }
+
+        Arrays.sort(products);
+        System.out.println("Posortowana: ");
+        for (Product p : products) {
+            System.out.println(p);
+        }
+    }
+}
