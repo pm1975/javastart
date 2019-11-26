8a4762304771187b5fb7eab2db47669db2763c99
pm1975 <pm1975@windowslive.com>
2019-11-26 22:28:14 +0100

    task 38 rekurencja (with error)

---
+++ 38-listy/src/task/app2.java
@@ -0,0 +1,60 @@
+package task;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ * Created by Piotr on 2019-11-26
+ */
+public class app2 {
+    public static void main(String[] args) {
+        System.out.println("Sumowanie liczb.");
+        Scanner sc = new Scanner(System.in);
+        ArrayList<Double> list = new ArrayList<>();
+        boolean tryAgain = true;
+        do {
+            try {
+                list = rekurencja(sc, list);
+            } catch (exitException e) {
+                if (e.getMessage() == "exit")
+                    tryAgain = false;
+                else System.out.println(e.getMessage());
+            }
+        } while (tryAgain);
+        double sum = 0;
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < list.size(); i++) {
+            sb.append(list.get(i) + " + ");
+            sum += list.get(i);
+        }
+        String text = sb.toString().substring(0, sb.length() - 3);
+        System.out.println(text + " = " + sum);
+        sc.close();
+    }
+
+    static ArrayList<Double> rekurencja(Scanner sc, ArrayList<Double> list) throws exitException {
+        String exit = null;
+        System.out.println("Podaj liczbę (exit = koniec):");
+        try {
+            exit = sc.next();
+            sc.nextLine();
+            double number = Double.parseDouble(exit);
+            list.add(number);
+            rekurencja(sc, list);
+        } catch (Exception e) {
+            if (exit.equals("exit")) {
+                throw new exitException("exit");
+            } else {
+                throw new exitException(exit + " to nie jest cyfra.");
+            }
+        }
+        return list;
+    }
+
+    static class exitException extends Exception {
+        public exitException(String message) {
+            super(message);
+        }
+    }
+
+}
