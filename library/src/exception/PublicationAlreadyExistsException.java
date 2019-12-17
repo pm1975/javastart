1c9c42a0fecb9edf6ea830f27b92245d4c8f0b81
pm1975 <pm1975@windowslive.com>
2019-12-17 15:21:13 +0100

    Library part 1 - about half

---
+++ library/src/exception/PublicationAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package exception;
+
+/**
+ * Created by Piotr on 2019-12-17
+ */
+public class PublicationAlreadyExistsException extends Exception {
+    public PublicationAlreadyExistsException(String message) { super(message);}
+}
---
+++ library/src/exception/UserAlreadyExistsException.java
@@ -0,0 +1,8 @@
+package exception;
+
+/**
+ * Created by Piotr on 2019-12-17
+ */
+public class UserAlreadyExistsException extends Exception {
+    public  UserAlreadyExistsException(String message) { super(message);}
+}
