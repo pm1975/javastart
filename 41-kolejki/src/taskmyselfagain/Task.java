9241b3de97a3915ce5686f011a1107e2d3bde9f7
PM <pm1975@windowslive.com>
2020-01-21 09:02:04 +0100

    done task 41 without help

---
+++ 41-kolejki/src/taskmyselfagain/Task.java
@@ -0,0 +1,53 @@
+package taskmyselfagain;
+
+import java.util.Comparator;
+
+public class Task implements Comparable<Task> {
+    private String name;
+    private String description;
+    private Priority priority;
+
+    public Task(String name, String description, Priority priority) {
+        this.name = name;
+        this.description = description;
+        this.priority = priority;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public Priority getPriority() {
+        return priority;
+    }
+
+    public void setPriority(Priority priority) {
+        this.priority = priority;
+    }
+
+    @Override
+    public String toString() {
+        return name + " - " + description + " - " + priority;
+    }
+
+    @Override
+    public int compareTo(Task t) {
+        return priority.compareTo(t.priority);
+    }
+
+    public enum Priority {
+        HIGH, MODERATE, LOW
+    }
+}
