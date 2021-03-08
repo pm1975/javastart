c1d09eff7616c8502f9a7a7b43c43712f3174cab
macosami <pm1975@windowslive.com>
2021-03-08 16:07:07 +0100

    11 Pamiec maszyny wirtualnej. Zarzadzenie pamiecia. cwiczenie 1. Polowa.

--- 12-pamiec-maszyny-wirtualnej/src/task1/Controller.java
+++ 12-pamiec-maszyny-wirtualnej/src/task1/Controller.java
@@ -1,5 +1,23 @@
 package task1;
 
 public class Controller {
+    void openWindows(Building building) {
+        building.firstRoom.window.open = true;
+        building.secondRoom.window.open = true;
+    }
 
+    void closeWindows(Building building) {
+        building.firstRoom.window.open = false;
+        building.secondRoom.window.open = false;
+    }
+
+    void openDoors(Building building) {
+        building.firstRoom.door.open = true;
+        building.secondRoom.door.open = true;
+    }
+
+    void closeDoors(Building building) {
+        building.firstRoom.door.open = false;
+        building.secondRoom.door.open = false;
+    }
 }
